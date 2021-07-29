b27eac2308c190c6f00b116d489148e9fe8f8b66
TimCurtis468 <tim_r_curtis@hotmail.com>
2021-07-29 10:57:28 +0100

    Added Frenzy buff

---
+++ Breakdown/Assets/Scripts/Frenzy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frenzy : Collectable
+{
+    // Start is called before the first frame update
+    protected override void ApplyEffect()
+    {
+        if ((Paddle.Instance != null) && (Paddle.Instance.PaddleIsTransforming == false))
+        {
+            Paddle.Instance.StartWidthAnimation(2.5f);
+        }
+        foreach (var ball in BallsManager.Instance.Balls)
+        {
+            ball.StartLightningBall();
+        }
+        try
+        {
+            int numBalls = BallsManager.Instance.Balls.Count - 1;
+            for (int idx = numBalls; idx >= 0; idx--)
+            //            foreach (Ball ball in BallsManager.Instance.Balls)
+            {
+                // Limit number of balls
+                if (BallsManager.Instance.Balls.Count < 10)
+                {
+                    var ball = BallsManager.Instance.Balls[idx];
+                    BallsManager.Instance.SpawnBalls(ball.gameObject.transform.position, 2, ball.isLightningBall);
+                }
+            }
+        }
+        catch
+        {
+
+        }
+    }
+}
